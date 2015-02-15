0acb0495030e009ecfffe9980747b94f56e850ae
Andrey Ivanov <ans.ivanov@gmail.com>
2015-02-15 17:11:03 +0300

    logging added

--- src/main/java/hh/homework/transactionexample/Main.java
+++ src/main/java/hh/homework/transactionexample/Main.java
@@ -4,27 +4,41 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import hh.homework.transactionexample.clubs.ClubService;
 import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 
 /**
  * Created by fib on 04/01/15.
  */
 class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
 
     public static void main(final String[] args) throws Exception {
+        log.info("app started...");
+
         final Injector injector = Guice.createInjector(new IocModule());
         final SessionFactory sessionFactory = injector.getInstance(SessionFactory.class);
 
+        final int playerId = 4;
+        final int clubCustomerId = 1;
+
+        MDC.put("playerId", Integer.toString(playerId));
+        MDC.put("clubCustomerId", Integer.toString(clubCustomerId));
+
         try {
-            if (args.length > 0 && "reinit".equals(args[0]))
+            if (args.length > 0 && "reinit".equals(args[0])) {
+                log.info("reinit db...");
                 HibernateUtils.initDb(sessionFactory.getCurrentSession());
-
+            }
             final ClubService clubService = injector.getInstance(ClubService.class);
-
-            clubService.SellPlayer(4, 1);
-            System.out.println("Игрок успешно продан");
+            clubService.SellPlayer(playerId, clubCustomerId);
+            log.info("игрок успешно продан");
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error("ошибка при продаже игрока", e);
         } finally {
+            log.info("app finished...");
+            MDC.clear();
             sessionFactory.close();
         }
     }
